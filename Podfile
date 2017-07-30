790f18989996d379b5a280ab8666fd98d16c6dc2
khalilacheche <achechekhalil@hotmail.fr>
2017-07-30 22:48:50 +0100

    Uploading files

---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Taxoum' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Taxoum
+
+  source 'https://github.com/CocoaPods/Specs.git'
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+  pod 'GooglePlacesAPI'
+  pod 'SwiftyJSON'
+
+ target 'TaxoumTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TaxoumUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Taxoum/ViewController.swift
+++ Taxoum/ViewController.swift
@@ -7,19 +7,301 @@
 //
 
 import UIKit
+import GoogleMaps
+import SwiftyJSON
+import GooglePlaces
+import GooglePlacesAPI
+import Alamofire
 
-class ViewController: UIViewController {
+enum Location {
+    case startLocation
+    case destinationLocation
+}
 
+class ViewController: UIViewController , GMSMapViewDelegate ,  CLLocationManagerDelegate {
+    
+    @IBOutlet weak var TaxiFare: UILabel!
+    @IBOutlet weak var googleMaps: GMSMapView!
+    @IBOutlet weak var startLocation: UITextField!
+    @IBOutlet weak var destinationLocation: UITextField!
+
+    
+    
+    var locationManager = CLLocationManager()
+    var locationSelected = Location.startLocation
+    
+    var locationStart = CLLocation()
+    var locationEnd = CLLocation()
+    let pricebykm = 750
+    let pricebyseconds = 30/9
+    var stm:String="Start location"
+    var dtm:String="End location"
+    var distanceinKM = 0
+    var timeInSeconds = 0
+    var mylocation :CLLocation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startMonitoringSignificantLocationChanges()
+        
+        //Your map initiation code
+        let camera = GMSCameraPosition.camera(withLatitude: -7.9293122, longitude: 112.5879156, zoom: 15.0)
+        
+        self.googleMaps.camera = camera
+        self.googleMaps.delegate = self
+        self.googleMaps?.isMyLocationEnabled = true
+        self.googleMaps.settings.myLocationButton = true
+        self.googleMaps.settings.compassButton = true
+        self.googleMaps.settings.zoomGestures = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: function for create a marker pin on map
+    func createMarker(titleMarker: String, /*iconMarker: UIImage,*/ latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(latitude, longitude)
+        marker.title = titleMarker
+        //marker.icon = iconMarker
+        marker.map = googleMaps
     }
+    
+    //MARK: - Location Manager delegates
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error to get location : \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        mylocation=location!
+        SelectStartLocation(place: location!, PlaceName: "Your Position")
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 17.0)
+        
+        self.googleMaps?.animate(to: camera)
+        self.locationManager.stopUpdatingLocation()
+        
+    }
+    
+    // MARK: - GMSMapViewDelegate
+    
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        googleMaps.isMyLocationEnabled = true
+    }
+    
+    func mapView(_ mapView: GMSMapView, willMove gesture: Bool) {
+        googleMaps.isMyLocationEnabled = true
+        
+        if (gesture) {
+            mapView.selectedMarker = nil
+        }
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        googleMaps.isMyLocationEnabled = true
+        return false
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        print("COORDINATE \(coordinate)") // when you tapped coordinate
+    }
+    
+    func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
+        googleMaps.isMyLocationEnabled = true
+        googleMaps.selectedMarker = nil
+        return false
+    }
+    
+    
+    
+    //MARK: - this is function for create direction path, from start location to desination location
+    func SelectStartLocation(place: CLLocation, PlaceName: String){
+        locationStart = place
+        createMarker(titleMarker: PlaceName,/* iconMarker: #imageLiteral(resourceName: "mapspin"),*/ latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+        stm = PlaceName
+        startLocation.text=PlaceName
+    }
+    func SelectEndLocation(place: CLLocation, PlaceName: String){
+        locationEnd = place
+        createMarker(titleMarker: PlaceName, /*iconMarker: #imageLiteral(resourceName: "mapspin"), */latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+        dtm = PlaceName
+        destinationLocation.text = PlaceName
+    }
+    func drawPath(startLocation: CLLocation, endLocation: CLLocation)
+    {
+        let origin = "\(startLocation.coordinate.latitude),\(startLocation.coordinate.longitude)"
+        let destination = "\(endLocation.coordinate.latitude),\(endLocation.coordinate.longitude)"
+        
+        
+        let url = "https://maps.googleapis.com/maps/api/directions/json?origin=\(origin)&destination=\(destination)&mode=driving"
+        //Requesting the routes
+        Alamofire.request(url).responseJSON { response in
+            
+            //print(response.request as Any)  // original URL request
+            //print(response.response as Any) // HTTP URL response
+            //print(response.data as Any)     // server data
+            //print(response.result as Any)   // result of response serialization
+            
+            let json = JSON(data: response.data!)
+            let routes = json["routes"].arrayValue
+            self.distanceinKM = json["routes"][0]["legs"][0]["distance"]["value"].intValue
+            self.timeInSeconds = json["routes"][0]["legs"][0]["duration"]["value"].intValue
+            print(self.distanceinKM)
+            print(self.timeInSeconds)
+            let TaxifarebyDist = 450 + self.distanceinKM/1000 * self.pricebykm
+            let TaxifarebyTime = 450 + self.timeInSeconds * self.pricebyseconds
+            print("Taxi fare by distance: ",TaxifarebyDist)
+            print("Taxi fare by time: ",TaxifarebyTime)
+            let finalTaxiFare = (TaxifarebyTime  +  TaxifarebyDist ) / 2
+            print ("Final Taxi Fare  ",finalTaxiFare)
+            self.TaxiFare.text = "Taxi Fare:  " + String(finalTaxiFare)
+            // print route using Polyline
+            for route in routes
+            {
+                let routeOverviewPolyline = route["overview_polyline"].dictionary
+                let points = routeOverviewPolyline?["points"]?.stringValue
+                let path = GMSPath.init(fromEncodedPath: points!)
+                let polyline = GMSPolyline.init(path: path)
+                polyline.strokeWidth = 4
+                polyline.strokeColor = UIColor.red
+                polyline.map = self.googleMaps
+            }
+            
+        }
+    }
+    
+    // MARK: when start location tap, this will open the search location
+    @IBAction func openStartLocation(_ sender: UIButton) {
+        
+        let autoCompleteController = GMSAutocompleteViewController()
+        autoCompleteController.delegate = self
+        
+        // selected location
+        locationSelected = .startLocation
+        
+        // Change text color
+        UISearchBar.appearance().setTextColor(color: UIColor.black)
+        self.locationManager.stopUpdatingLocation()
+        
+        self.present(autoCompleteController, animated: true, completion: nil)
+    }
+    
+    // MARK: when destination location tap, this will open the search location
+    @IBAction func openDestinationLocation(_ sender: UIButton) {
+        
+        let autoCompleteController = GMSAutocompleteViewController()
+        autoCompleteController.delegate = self
+        
+        // selected location
+        locationSelected = .destinationLocation
+        
+        // Change text color
+        UISearchBar.appearance().setTextColor(color: UIColor.black)
+        self.locationManager.stopUpdatingLocation()
+        
+        self.present(autoCompleteController, animated: true, completion: nil)
+    }
+    
+    @IBAction func SwitchPlaces(_ sender: Any) {
+        let newStart = locationEnd
+        locationEnd = locationStart
+        locationStart = newStart
+        let newstm = dtm
+        dtm = stm
+        stm = newstm
+        SelectStartLocation(place: locationStart, PlaceName: stm)
+        SelectEndLocation(place: locationEnd, PlaceName: dtm)
+        
+    }
+    @IBAction func selectMyPositionAtStart(_ sender: Any) {
+    SelectStartLocation(place: mylocation, PlaceName: "My Position")
+    }
+    @IBAction func selectMyPositionAtDestination(_ sender: Any) {
+    SelectEndLocation(place: mylocation, PlaceName: "My Position")
+    }
+    
+    // MARK: SHOW DIRECTION WITH BUTTON
+    @IBAction func showDirection(_ sender: UIButton) {
+        // when button direction tapped, must call drawpath func
+        googleMaps.clear()
+        self.drawPath(startLocation: locationStart, endLocation: locationEnd)
+        createMarker(titleMarker: stm, /*iconMarker: #imageLiteral(resourceName: "mapspin") ,*/ latitude: (locationStart.coordinate.latitude), longitude: (locationStart.coordinate.longitude))
+        createMarker(titleMarker: dtm,/* iconMarker: #imageLiteral(resourceName: "mapspin"),*/ latitude: locationEnd.coordinate.latitude, longitude: locationEnd.coordinate.longitude)
+    }
+    
+}
 
+// MARK: - GMS Auto Complete Delegate, for autocomplete search location
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error \(error)")
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        
+        // Change map location
+        let camera = GMSCameraPosition.camera(withLatitude: place.coordinate.latitude, longitude: place.coordinate.longitude, zoom: 16.0
+        )
+        
+        
+        // set coordinate to text
+        if locationSelected == .startLocation {
+            let location =  CLLocation(latitude: place.coordinate.latitude,longitude: place.coordinate.longitude)
+            SelectStartLocation(place: location, PlaceName: place.name)
+        } else {
+            let location =  CLLocation(latitude: place.coordinate.latitude,longitude: place.coordinate.longitude)
+            SelectEndLocation(place: location, PlaceName: place.name)        }
+        /*CLGeocoder().reverseGeocodeLocation(locationStart, completionHandler: {(placemarks, error) -> Void in
+            print(self.locationStart)
+            
+            if error != nil {
+                print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
+                return
+            }
+            
+            if (placemarks?.count)! > 0 {
+                let pm = placemarks?[0]
+                self.stm = (pm?.name)!
+                
+            }
+            else {
+                print("Problem with the data received from geocoder")
+            }
+        })*/
+        
+        self.googleMaps.camera = camera
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+}
 
+public extension UISearchBar {
+    
+    public func setTextColor(color: UIColor) {
+        let svs = subviews.flatMap { $0.subviews }
+        guard let tf = (svs.filter { $0 is UITextField }).first as? UITextField else { return }
+        tf.textColor = color
+    }
+    
 }
 
