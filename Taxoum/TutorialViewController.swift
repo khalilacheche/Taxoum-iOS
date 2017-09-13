26b5b05b6ccf243eb25795e7603dbeccd54876ec
Amir Braham <amirbrahamm@gmail.com>
2017-09-13 16:29:15 +0100

    Added a new design mockup and some minor changes to tutorial view controller

--- Taxoum/TutorialViewController.swift
+++ Taxoum/TutorialViewController.swift
@@ -13,24 +13,20 @@ class TutorialViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         let taxiAnimationView = LOTAnimationView(name: "TaxiAnimation")
         self.view.addSubview(taxiAnimationView)
         taxiAnimationView.contentMode = .scaleAspectFill
-        taxiAnimationView.frame =  CGRect(x: 0 , y: 140, width: self.view.frame.size.width,height: 215)
+        taxiAnimationView.frame =  CGRect(x: 0 , y: 337, width: self.view.frame.size.width,height: 215)
         taxiAnimationView.loopAnimation = true
         taxiAnimationView.play()
-        
-        let swipeAnimationView = LOTAnimationView(name: "swipe_up")
-        self.view.addSubview(swipeAnimationView)
-        swipeAnimationView.contentMode = .scaleAspectFill
-        swipeAnimationView.frame =  CGRect(x: 145 , y: 450, width: 90,height: 45)
-        swipeAnimationView.transform = CGAffineTransform(rotationAngle: 1.5708);
-        swipeAnimationView.loopAnimation = true
-        swipeAnimationView.play()
 
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
