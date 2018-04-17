af185be679f474095df9b0e953a84945b5154d73
Amir Braham <amirbrahamm@gmail.com>
2018-04-17 11:59:41 +0100

    ..

--- Taxoum/ViewController.swift
+++ Taxoum/ViewController.swift
@@ -246,7 +246,7 @@ class ViewController: UIViewController , GMSMapViewDelegate ,  CLLocationManager
         
         // Set message title, body, and icon. Here, we're overriding the default warning
         // image with an emoji character.
-        let iconText = ["🙌", "✅", "👌"].sm_random()!
+        let iconText = ["🙌", "👌","💰","💵"].sm_random()!
         view.configureContent(title: "Taxi Fare", body: String(finalTaxiFare) + " TND", iconText: iconText)
         
         var SwiftMessageConfig =  SwiftMessages.Config()
